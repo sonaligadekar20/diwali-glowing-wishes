c55eb631bbd780cc2086a1b9cab269dea56a94cc
sonaligadekar20 <sonaliygadekar@gmail.com>
2023-11-07 23:13:32 +0530

    added diwali wishes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,4 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import Home from './views/Home/Home.jsx';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
@@ -8,10 +6,10 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home/>
+    element: <Home/>,
   }
 ])
 
-const root = ReactDOM.createRoot(document.getElementById)
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />)
+const root = ReactDOM.createRoot(document.getElementById ('root'));
+
+root.render( <RouterProvider router={router} />)
